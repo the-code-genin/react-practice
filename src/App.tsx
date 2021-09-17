c4e659f99c5b8c4cdd2e4eabd0637ea4ac320313
Mohammed Adekunle <adekunle3317@gmail.com>
2021-09-17 04:24:59 +0100

    Updated props

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export interface Props {
+  appName?: string
+};
+
+export default class App extends Component {
+  public props: Props;
+
+  constructor(props: Props) {
+    super(props);
+
+    // Initialize props
+    this.props = props
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.props.appName}</p>
+        </header>
+      </div>
+    );
+  }
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App appName="The Start Of A New Era" />
   </React.StrictMode>,
   document.getElementById('root')
 );
