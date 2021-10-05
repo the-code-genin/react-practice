b87f3da4c76919f4c93a1c1bfbea6b19c0cac79d
Mohammed Adekunle <adekunle3317@gmail.com>
2021-10-05 02:35:42 +0100

    Updated routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,18 +2,20 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import About from "../pages/About";
 import Home from "../pages/Home";
+import Movie from "../pages/Movie";
+import NotFound from "../pages/NotFound";
 
 export default class Routes extends React.Component {
     render() {
         return (
             <Switch>
-                <Route exact path="/">
-                    <Home />
-                </Route>
-    
-                <Route path="/about">
-                    <About />
-                </Route>
+                <Route exact path="/" component={Home} />
+
+                <Route path="/about" component={About} />
+
+                <Route path="/movies/:movie" component={Movie} />
+
+                <Route path="*" component={NotFound} />
             </Switch>
         );
     }
