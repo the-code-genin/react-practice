4268ecf382fdddb94c7a5fb9cabd8a7fea3aa4e5
Mohammed Adekunle <adekunle3317@gmail.com>
2021-10-05 02:33:20 +0100

    Refactored home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,32 @@
 import React from "react";
 import "../assets/css/style.css";
 import Navbar from "../components/Navbar";
+import movies from "../assets/movies.json";
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
+  buildMovies() {
+    return movies.map(movie => {
+      const link = `/movies/${movie.slug}`;
+      return (
+        <Link to={link}>
+          <h1>{movie.name}</h1>
+          <p>{movie.description}</p>
+        </Link>
+      );
+    });
+  }
+
   render() {
     return (
       <div className="container">
         <Navbar/>
-        <h1>Hello World</h1>
+
+        <h1>Home Page</h1>
+
+        <div>
+          {this.buildMovies()}
+        </div>
       </div>
     );
   }
